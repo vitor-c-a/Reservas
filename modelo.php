24a07a1cef935fe6494e060e2477957b26a3d178
VITOR CAMARGO ARAUJO <vitor.caraujo3@senacsp.edu.br>
2024-08-02 21:02:37 -0300

    Modelo de navegacao

---
+++ modelo.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="perfil.css">
+    <title>Perfil</title>
+</head>
+<body>
+
+    <header>
+        <div id="quadro_logo">
+            <img id="logo" src="./img/logo.png" alt="Logo do restaurante">
+        </div>
+        
+        <p id="endereco">Endereço: Alameda Clélio Rodrigo, 159 - Vila Mariana</p>
+        <nav id="navegacao">
+            <ul class="lista">
+                <li class="item_lista"><img src="./img/icone_restaurante.png" alt="Icone da aba da lista restaurante"> Restaurante</li>
+                <li class="item_lista"><img src="./img/icone_reserva.png" alt="Icone da aba reservar"> Reservar</li>
+                <li class="item_lista"><img src="./img/icone_historico.png" alt="Icone da aba histórico"> Histórico</li>
+                <li class="item_lista"><img src="./img/icone_perfil.png" alt="Icone da aba perfil"> Perfil</li>
+                <li class="item_lista"><img src="./img/icone_sair.png" alt="Icone da aba perfil"> Sair</li>
+            </ul>
+        </nav>
+    </header>
+
+    <main hidden>
+        <h1 class="titulo">Perfil</h1>
+        <form method="post">
+                <div class="dados_perfil">
+                    <div class="dados_itens">
+                        <label for="nome">Nome:</label>
+                        <input type="text" name="nome" id="nome">
+                    </div>
+
+                    <div class="dados_itens">
+                        <label for="cpf">CPF:</label>
+                        <input type="text" name="nome" id="cpf">
+                    </div>
+                    
+                    <div class="dados_itens">
+                        <label for="email">E-mail:</label>
+                        <input type="text" name="email" id="email">
+                    </div>
+
+                    <div class="dados_itens">
+                        <label for="telefone">Telefone</label>
+                        <input type="text" name="telefone" id="telefone">
+                    </div>
+
+                    <div class="dados_itens">
+                        <label for="reserva_ativa">Reservas ativas:</label>
+                        <div id="reserva_ativa"></div>
+                    </div>
+
+                    <div class="dados_itens">
+                        <label for="reserva_inativa">Reservas inativas:</label>
+                        <div id="reserva_inativa"></div>
+                    </div>
+            </div>
+            <button>Alterar</button>
+            <button hidden>Cancelar</button>
+            <button hidden>Concluir</button>
+        </form>
+    </main>
+
+    <footer>
+        
+    </footer>
+    
+</body>
+</html>
