859b62be88c1adcfa3c9bbaf853746e499e0f98b
VITOR CAMARGO ARAUJO <vitor.caraujo3@senacsp.edu.br>
2024-08-07 20:21:44 -0300

    Iniciando pagina de agendamento

---
+++ modelo_navegacao.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./modelo_navegacao.css">
+    <title>Perfil</title>
+</head>
+<body>
+
+    <header>
+        <div id="quadro_logo">
+            <img id="logo" src="./img/logo.png" alt="Logo do restaurante">
+        </div>
+        
+        <p id="endereco">Endereço: Alameda Clélio Rodrigo, 159 - Vila Mariana</p>
+        <nav id="navegacao">
+            <ul class="lista">
+                <li class="item_lista"><img src="./img/icone_restaurante.png" alt="Icone da aba da lista restaurante"> Restaurante</li>
+                <li class="item_lista"><img src="./img/icone_reserva.png" alt="Icone da aba reservar"> Reservar</li>
+                <li class="item_lista"><img src="./img/icone_historico.png" alt="Icone da aba histórico"> Histórico</li>
+                <li class="item_lista"><img src="./img/icone_perfil.png" alt="Icone da aba perfil"> Perfil</li>
+                <li class="item_lista"><img src="./img/icone_sair.png" alt="Icone da aba perfil"> Sair</li>
+            </ul>
+        </nav>
+    </header>
+
+    
+    
+</body>
+</html>
