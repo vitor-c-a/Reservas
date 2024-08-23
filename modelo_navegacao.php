91a69973e4c52e9305ea05feb144a0c0e1744267
Vitor Camargo <89257098+vitor-c-a@users.noreply.github.com>
2024-08-22 21:06:44 -0300

    Update modelo_navegacao.php

--- modelo_navegacao.php
+++ modelo_navegacao.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./modelo_navegacao.css">
+    <link rel="stylesheet" href="./modelo_navegacao_estilo.css">
     <title>Modelo de Navegação</title>
 </head>
 <body>
